5418005b92e75bcfc06cefca627dfdccb7aa1341
Imran hassan <72485040+imran123509@users.noreply.github.com>
2021-12-08 23:45:49 +0530

    Add files via upload

---
+++ minimum num reduce to 1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+/*
+recursion relation
+f(n)=min(f(n-1), f(n/2),f(n/3))+1
+f(1)=0;
+*/
+int n;
+cin>>n;
+int dp[n+1];
+dp[1]=0;
+for(int i=2; i<=n; i++){
+	dp[i]=dp[i-1]+1;
+	if(i%2==0) dp[i]=min(dp[i],dp[i/2]+1);
+	if(i%3==0) dp[i]=min(dp[i], dp[i/3]+1);
+}
+cout<<dp[n]<<endl;
+}
